f39a3e5cf6a4676a153e3eb853dd67a606901cd6
ILCapoMoi <moises180juegos@gmail.com>
2025-07-13 01:44:42 +0200

    Update grayboty.py

--- grayboty.py
+++ grayboty.py
@@ -126,7 +126,7 @@ POINT_VALUES = {"mvp": 3, "promo": 2, "attended": 1}
 SABERFORCE_BADGE_ID = 480453722785205
 OG_ROLE_NAME = "OG"
 OG_FECHA_INICIO = datetime(2024, 11, 10)
-OG_FECHA_FIN = datetime(2025, 07, 12)
+OG_FECHA_FIN = datetime(2025, 7, 12)
 
 # ───────────── Bot setup ─────────────
 intents = discord.Intents.default()
