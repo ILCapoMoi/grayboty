980a309582612eff163a3788c90423e4c1504f93
ILCapoMoi <moises180juegos@gmail.com>
2025-07-18 18:37:22 +0200

    Update grayboty.py

--- grayboty.py
+++ grayboty.py
@@ -213,7 +213,7 @@ async def showprofile(interaction: discord.Interaction, member: discord.Member |
 
     embed.add_field(name="Training Points", value=doc.get("tp", 0), inline=True)
     embed.add_field(name="Mission Points", value=doc.get("mp", 0), inline=True)
-    embed.add_field(name="────────────", value="\u200b", inline=False)
+    embed.add_field(name="<:H1Laser:1395749428135985333><:H2Laser:1395749449753563209><:R1Laser:1395746456681578628><:R1Laser:1395746456681578628><:R1Laser:1395746456681578628><:R1Laser:1395746456681578628><:R1Laser:1395746456681578628><:R1Laser:1395746456681578628><:R2Laser:1395746474293198949>", value="\u200b", inline=False)
     embed.add_field(name="Rank", value=highest_rank_raw, inline=False)
 
     next_rank = None
