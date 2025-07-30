28a54b9db3cb8b7b9e567d832301d5d0f2d984e8
ILCapoMoi <moises180juegos@gmail.com>
2025-07-30 15:36:20 +0200

    Update grayboty.py

--- grayboty.py
+++ grayboty.py
@@ -270,6 +270,7 @@ async def showprofile(interaction: discord.Interaction, member: discord.Member |
     embed.add_field(name="\u200b", value="\u200b", inline=True)
     embed.add_field(name="**War Points**", value=doc.get("wp", 0), inline=True)
     embed.add_field(name="**Raid Points**", value=doc.get("rp", 0), inline=True)
+    embed.add_field(name="\u200b", value="\u200b", inline=True)
 
 
     embed.add_field(
