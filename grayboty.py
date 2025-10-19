0a582be077b000a25906a5506a4abd04e33b5b6f
ILCapoMoi <moises180juegos@gmail.com>
2025-10-19 11:43:53 +0200

    Update grayboty.py

--- grayboty.py
+++ grayboty.py
@@ -261,21 +261,21 @@ async def showprofile(interaction: discord.Interaction, member: discord.Member |
         {
             "id": 1413828641397149716,  # Emeritus Emperor
             "name": "Emeritus Emperor",
-            "subtitle": "",
+            "subtitle": "\u200b",
             "emoji": "<:RetiredTGO:1429142210301005904>",
             "text": "Once crowned, forever eternal."
         },
         {
             "id": 1413829540987277332,  # Elder of Council
             "name": "Elder of Council",
-            "subtitle": "",
+            "subtitle": "\u200b",
             "emoji": "<:RetiredCo:1413856505987596380>",
             "text": "Their wisdom echoes in every council hall."
         },
         {
             "id": 1381562883803971605,  # Retired HR
             "name": "Retired",
-            "subtitle": "",
+            "subtitle": "\u200b",
             "emoji": "<:RetiredHR:1413856468595114056>",
             "text": "Their honor endures beyond their service."
         }
@@ -1243,6 +1243,7 @@ except Exception as e:
     import traceback
     traceback.print_exc()
     sys.exit(1)
+
 
 
 
